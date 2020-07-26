742387cd36bfd2be7361fcb480f42ef8d4a22c29
rachelcrevoisier <rachelcrevoisier@videotron.ca>
2020-07-26 16:20:29 -0400

    Insertion ajaxGet

--- produit.js
+++ produit.js
@@ -1,2 +1,6 @@
+// connexion avec le fichier connexion.js
+ajaxGet("http://localhost:3000/api/teddies", function (reponse) {
+    var listeOursons = JSON.parse(reponse);
+
 var idProduit = $_GET('produit');
     console.log (idProduit);
