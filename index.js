32bf5ce6f3e8bc9d8126e3b8ee3097bcce87712f
rachelcrevoisier <rachelcrevoisier@videotron.ca>
2020-07-26 15:52:41 -0400

    Creation liste des produits sur la première page

--- index.js
+++ index.js
@@ -1,11 +1,48 @@
+// connexion avec le fichier connexion.js
 ajaxGet("http://localhost:3000/api/teddies", function (reponse) {
-    var tableaux = JSON.parse(reponse);
-    var tableauxElt = document.getElementById("tableaux");
-    tableaux.forEach(function (tableau) {
-        // Crée une ligne de tableau HTML pour chaque tableau
-        var ligneElt = document.createElement("tr");
-        ligneElt.innerHTML = "<td>" + tableau.imageUrl + "</td>"+"<td>" + tableau.name + "</td>" + "<td>" + tableau.price + "</td>" +
-            "<td>" + tableau.description + "</td>";
-        tableauxElt.appendChild(ligneElt);
+    var listeOursons = JSON.parse(reponse);
+    // Placement au 3ème row
+    var EltRow = document.getElementsByClassName("row");
+    var rowCard = EltRow[2];
+    
+    
+       
+        listeOursons.forEach(function (listeProduits) {
+            
+            var divCol = document.createElement("div");
+            divCol.setAttribute("class", "col-12 col-lg-4");
+                rowCard.appendChild(divCol);
+                // Création div card
+                var divCard = document.createElement("div");
+                divCard.setAttribute("class", "card");
+                divCol.appendChild(divCard);
+                
+                var cardBody = document.createElement("div");
+                cardBody.setAttribute("class", "card-body");
+                divCard.appendChild(cardBody);
+
+                var lienOurson = document.createElement("a");
+                lienOurson.href = "https://rachelcrevoisier.github.io/P5_Orinoco/produit?produit=" + listeProduits._id;
+                cardBody.appendChild(lienOurson);
+        var imageOurson = document.createElement("img");
+        imageOurson.setAttribute("class", "card-img-top");
+        
+        imageOurson.src = listeProduits.imageUrl;
+        imageOurson.setAttribute("width",40);
+        imageOurson.setAttribute("Alt", "L'ourson" + listeProduits.name);
+        lienOurson.appendChild(imageOurson);
+        
+        var titleOurson = document.createElement("h5");
+        titleOurson.setAttribute("class", "card-title");
+        titleOurson.innerHTML = listeProduits.name;
+        cardBody.appendChild(titleOurson);
+        var descriptionOurson = document.createElement("p");
+        descriptionOurson.setAttribute("class", "card-text");
+        descriptionOurson.innerHTML = listeProduits.description;
+        cardBody.appendChild(descriptionOurson);
+        var prixOurson = document.createElement("p");
+        prixOurson.setAttribute("class", "price");
+        prixOurson.innerHTML = listeProduits.price + "€";
+        cardBody.appendChild(prixOurson);
     });
 });
