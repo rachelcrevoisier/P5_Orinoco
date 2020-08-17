1b661d5a255d22b889ea9c004ced9f916cf4beae
rachelcrevoisier <rachelcrevoisier@videotron.ca>
2020-08-17 10:53:59 -0400

    Affichage du produit selectionné sur la page produit

--- index.js
+++ index.js
@@ -4,14 +4,11 @@ ajaxGet("http://localhost:3000/api/teddies", function (reponse) {
     // Placement au 3ème row
     var EltRow = document.getElementsByClassName("row");
     var rowCard = EltRow[2];
-    
-    
-       
+    // création de la liste des produits avec la mise en page card
         listeOursons.forEach(function (listeProduits) {
-            
             var divCol = document.createElement("div");
             divCol.setAttribute("class", "col-12 col-lg-4");
-                rowCard.appendChild(divCol);
+            rowCard.appendChild(divCol);
                 // Création div card
                 var divCard = document.createElement("div");
                 divCard.setAttribute("class", "card");
@@ -22,27 +19,30 @@ ajaxGet("http://localhost:3000/api/teddies", function (reponse) {
                 divCard.appendChild(cardBody);
 
                 var lienOurson = document.createElement("a");
-                lienOurson.href = "https://rachelcrevoisier.github.io/P5_Orinoco/produit.html?produit=" + listeProduits._id;
+                lienOurson.href = "produit.html?produit=" + listeProduits._id;
                 cardBody.appendChild(lienOurson);
-        var imageOurson = document.createElement("img");
-        imageOurson.setAttribute("class", "card-img-top");
         
-        imageOurson.src = listeProduits.imageUrl;
-        imageOurson.setAttribute("width",40);
-        imageOurson.setAttribute("Alt", "L'ourson" + listeProduits.name);
-        lienOurson.appendChild(imageOurson);
+                var imageOurson = document.createElement("img");
+                imageOurson.setAttribute("class", "card-img-top");
         
-        var titleOurson = document.createElement("h5");
-        titleOurson.setAttribute("class", "card-title");
-        titleOurson.innerHTML = listeProduits.name;
-        cardBody.appendChild(titleOurson);
-        var descriptionOurson = document.createElement("p");
-        descriptionOurson.setAttribute("class", "card-text");
-        descriptionOurson.innerHTML = listeProduits.description;
-        cardBody.appendChild(descriptionOurson);
-        var prixOurson = document.createElement("p");
-        prixOurson.setAttribute("class", "price");
-        prixOurson.innerHTML = listeProduits.price + "€";
-        cardBody.appendChild(prixOurson);
-    });
+                imageOurson.src = listeProduits.imageUrl;
+                imageOurson.setAttribute("width",40);
+                imageOurson.setAttribute("Alt", "L'ourson" + listeProduits.name);
+                lienOurson.appendChild(imageOurson);
+        
+                var titleOurson = document.createElement("h5");
+                titleOurson.setAttribute("class", "card-title");
+                titleOurson.innerHTML = listeProduits.name;
+                cardBody.appendChild(titleOurson);
+        
+                var descriptionOurson = document.createElement("p");
+                descriptionOurson.setAttribute("class", "card-text");
+                descriptionOurson.innerHTML = listeProduits.description;
+                cardBody.appendChild(descriptionOurson);
+        
+                var prixOurson = document.createElement("p");
+                prixOurson.setAttribute("class", "price");
+                prixOurson.innerHTML = listeProduits.price + "€";
+                cardBody.appendChild(prixOurson);
+            });
 });
--- produit.js
+++ produit.js
@@ -4,32 +4,97 @@ function idProduit (sVar) {
   }
   
   idProduit = (idProduit("produit"));
-  console.log(idProduit);
+  
 
   ajaxGet("http://localhost:3000/api/teddies", function (reponse) {
     var oursonSelectionne = JSON.parse(reponse);
-    var EltTab = document.getElementsByClassName("tableaux");
-    var rowtab = EltTab[0];
-    var creaTable = document.createElement("table");
-    creaTable.setAttribute("class", "col-12 col-lg-4");
-        rowtab.appendChild(creaTable);
+    var EltRow = document.getElementsByClassName("row");
+    var rowCard = EltRow[2];
         
     oursonSelectionne.forEach(function (ourson) {
         if (idProduit === ourson._id){
-          console.log("Tout fonctionne");
-          var imageOurson = document.createElement("img");
-              
-        imageOurson.src = ourson.imageUrl;
-        imageOurson.setAttribute("width",40);
-        imageOurson.setAttribute("Alt", "L'ourson" + ourson.name);
-        rowtab.appendChild(imageOurson);
-          var ligneElt = document.createElement("tr");
-        ligneElt.innerHTML = "<td>" + ourson.imageUrl + "</td>"+"<td>" + ourson.name + "</td>" + "<td>" + ourson.price + "</td>" +
-            "<td>" + ourson.description + "</td>";
-        creaTable.appendChild(ligneElt);
+            var divCol = document.createElement("div");
+            divCol.setAttribute("class", "col-12 col-lg-5");
+            rowCard.appendChild(divCol);
+                var imageOurson = document.createElement("img");
+                imageOurson.src = ourson.imageUrl;
+                imageOurson.setAttribute("width",350);
+                imageOurson.setAttribute("Alt", "L'ourson" + ourson.name);
+            divCol.appendChild(imageOurson);
+            var colDescription = document.createElement("div");
+            colDescription.setAttribute("class", "col-12 col-lg-7");
+            rowCard.appendChild(colDescription);
+                var titleOurson = document.createElement("h5");
+                titleOurson.setAttribute("class", "nomOurson");
+                titleOurson.innerHTML = ourson.name;
+                colDescription.appendChild(titleOurson);
+                
+                var descriptionOurson = document.createElement("p");
+                descriptionOurson.innerHTML = ourson.description;
+                colDescription.appendChild(descriptionOurson);
 
-        }else{
-          console.log("Nous avons rencontré un problème dans le choix de votre produit. Veuillez recommencer.");
+                var couleurOursonP = document.createElement("p");
+                couleurOursonP.textContent = "Sélectionnez une couleur ";
+                colDescription.appendChild(couleurOursonP);
+
+                var sautDeLigneDescription = document.createElement("br");
+                couleurOursonP.appendChild(sautDeLigneDescription);
+                
+                var couleurOurson = document.createElement("select");
+                couleurOurson.setAttribute("class", "selectpicker");
+                couleurOursonP.appendChild(couleurOurson);
+                                
+                var couleurs = ourson.colors;
+                for (var i = 0; i < couleurs.length; i++) {
+                    var optionCouleur = document.createElement("option");
+                    optionCouleur.setAttribute("class", "couleurOurson");
+                    optionCouleur.textContent = couleurs[i];
+                    couleurOurson.appendChild(optionCouleur);
+                    };
+                
+                var prixOurson = document.createElement("p");
+                prixOurson.setAttribute("class", "prixOurson");
+                prixOurson.innerHTML = "Prix : " + ourson.price + " €";
+                colDescription.appendChild(prixOurson);
+
+                var quantite = document.createElement("p");
+                quantite.textContent = "Quantite ";
+                colDescription.appendChild(quantite);
+                var quantitechoix = document.createElement("select");
+                quantitechoix.setAttribute("class", "selectpicker");
+                quantite.appendChild(quantitechoix);
+                    var optionchoix1 = document.createElement("option");
+                    optionchoix1.setAttribute("class", "quantiteOurson");
+                    optionchoix1.textContent = "1";
+                    quantitechoix.appendChild(optionchoix1);
+                    var optionchoix2 = document.createElement("option");
+                    optionchoix2.setAttribute("class", "quantiteOurson");
+                    optionchoix2.textContent = "2";
+                    quantitechoix.appendChild(optionchoix2);
+                    var optionchoix3 = document.createElement("option");
+                    optionchoix3.setAttribute("class", "quantiteOurson");
+                    optionchoix3.textContent = "3";
+                    quantitechoix.appendChild(optionchoix3);
+                    var optionchoix4 = document.createElement("option");
+                    optionchoix4.setAttribute("class", "quantiteOurson");
+                    optionchoix4.textContent = "4";
+                    quantitechoix.appendChild(optionchoix4);
+                    var optionchoix5 = document.createElement("option");
+                    optionchoix5.setAttribute("class", "quantiteOurson");
+                    optionchoix5.textContent = "5";
+                    quantitechoix.appendChild(optionchoix5);
+
+                var boutonAjoutPanier = document.createElement("button");
+                boutonAjoutPanier.setAttribute("type", "button");
+                boutonAjoutPanier.setAttribute("class", "btn btn-info info ajoutPanier");
+                boutonAjoutPanier.textContent = "Ajouter au panier";
+                colDescription.appendChild(boutonAjoutPanier);
+                var sautDeLigneBouton = document.createElement("br");
+                colDescription.appendChild(sautDeLigneBouton);
+                var sautDeLigneBouton = document.createElement("br");
+                colDescription.appendChild(sautDeLigneBouton);
+                
+
         }
     })
 });
